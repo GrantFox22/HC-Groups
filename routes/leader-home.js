eb11e9e1dd070a8b3cc3fb55a44005ab40299d1b
GrantFox22 <guitarg85@gmail.com>
2019-12-30 11:36:19 -0600

    added password meter and confirm password code verification logic to register page.

--- routes/leader-home.js
+++ routes/leader-home.js
@@ -5,6 +5,9 @@ const router = express.Router()
 
 /* GET leader home page. */
 router.get('/', async function (req, res, next) {
+  if (!req.session.user) {
+    res.redirect('login')
+  }
   res.render('leader-home', { user: 'grant.fox' })
 })
 
--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,24 @@
 require('dotenv').config()
 const debug = require('../config/debug')('app:register')
+const PasswordValidator = require('password-validator')
 const express = require('express')
 const hcDao = require('../repository/hc-dao')
 const bcrypt = require('bcrypt')
 const router = express.Router()
+const schema = new PasswordValidator()
+
+schema
+  .is().min(8) // Minimum length 8
+  .is().max(100) // Maximum length 100
+  .has().uppercase() // Must have uppercase letters
+  .has().lowercase() // Must have lowercase letters
+  .has().digits() // Must have digits
+  .has().symbols() // Must have symbols
+  .has().not().spaces() // Should not have spaces
 
 /* GET register page. */
 router.get('/', async function (req, res, next) {
-  res.render('register', { user: 'grant.fox' })
+  res.render('register', { passwordValidator: schema })
 })
 
 router.post('/', async function (req, res, next) {
